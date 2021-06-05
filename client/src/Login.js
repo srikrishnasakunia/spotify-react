f371df201e1258fb18ced746c0a108819b148461
srikrishnasakunia <43899917+srikrishnasakunia@users.noreply.github.com>
2021-06-05 12:10:09 +0530

    Update Login.js
    Use your own client ID, which can be found on your Dashboard of spotify developers account.

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=87274b8b2e5e4ba1915fb48d07fc11bd&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=**INSERT YOUR OWN**&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 export default function Login() {
     return (
